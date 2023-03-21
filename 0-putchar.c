80094bc1a098bc07c765f611703f553619677f1c
Loubnaa1 <loubnamerzak4@gmail.com>
2023-03-21 15:50:00 -0700

    My first commit

---
+++ 0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char Array[] = "_putchar";
+		for (int i = 0; i < sizeof(Array)-1; i++)
+		{
+			_putchar(char Array[i]);
+			_putchar('\n');
+		}
+}
