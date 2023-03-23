13c36525e28fe9e8b899fcf23aff6742e6030071
Loubnaa1 <loubnamerzak4@gmail.com>
2023-03-22 18:44:24 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_abs - function that computes the absolute value of an integer
+ *Return: -n (if negative) n if positive
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_last_digit -  function that prints the last digit of a number
+ * @n:this is the checking  number
+ * Return: b (always positive)
+ */
+int print_last_digit(int n)
+{
+	int b;
+
+	b = n % 10;
+	if (b < 0)
+	{
+		b = -b;
+		_putchar(b + '0');
+		return (b);
+	}
+	else
+	{
+		_putchar(b + '0');
+		return (b);
+	}
+}
