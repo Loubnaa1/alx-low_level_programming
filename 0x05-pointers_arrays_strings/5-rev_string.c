0cf06ba0b17414690dc16a3d77da53e07bb677a7
Loubnaa1 <loubnamerzak4@gmail.com>
2023-03-27 17:01:42 -0700

    My first commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,17 +6,23 @@
  */
 void rev_string(char *s)
 {
-	int i = 0;
-	int n = 0;
-	char va;
+	char tmp;
+	int i, len, len1;
 
-	while (s[i] != '\0')
-		i++;
-	for (n = 0; n < i - 1; n++)
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
 	{
-		va = s[n];
-		s[n] = s[i];
-		s[i] = va;
-		i--;
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }
