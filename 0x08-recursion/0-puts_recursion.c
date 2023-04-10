3ea7b0c2c087837eb91d07751a7a1d937f164623
Loubnaa1 <loubnamerzak4@gmail.com>
2023-04-10 16:49:39 -0700

    My first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line
+ *
+ * @s: the caracter to print
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (*(s + i) != '\0')
+	{
+		_putchar(*(s + i));
+		i++;
+		_puts_recursion(s + i);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
