141de98aec48033b4289e38fb3826474b241ff0c
Loubnaa1 <Loubnamerzak4@gmail.com>
2023-07-29 14:55:05 -0700

    mycommit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_binary - prints the binary representation
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int variable;
+	int i = 0, j;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+	variable = n;
+	while (variable != 0)
+	{
+		variable = variable >> 1;
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		if ((n >> j) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
