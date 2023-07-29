383143d6612918cfb06dded1753289018fc495b7
Loubnaa1 <Loubnamerzak4@gmail.com>
2023-07-29 16:28:53 -0700

    mycommit

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -273,10 +273,10 @@ void close_elf(int elf)
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
-	int o, r;
+	int file_dir, result;
 
-	o = open(argv[1], O_RDONLY);
-	if (o == -1)
+	file_dir = open(argv[1], O_RDONLY);
+	if (file_dir == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
@@ -284,15 +284,15 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	header = malloc(sizeof(Elf64_Ehdr));
 	if (header == NULL)
 	{
-		close_elf(o);
+		close_elf(file_dir);
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
-	r = read(o, header, sizeof(Elf64_Ehdr));
-	if (r == -1)
+	result = read(file_dir, header, sizeof(Elf64_Ehdr));
+	if (result == -1)
 	{
 		free(header);
-		close_elf(o);
+		close_elf(file_dir);
 		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 		exit(98);
 	}
@@ -309,6 +309,6 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
-	close_elf(o);
+	close_elf(file_dir);
 	return (0);
 }
