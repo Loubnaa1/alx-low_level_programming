d202a629d3448c66ad81b07f80447f26695a563f
Loubnaa1 <loubnamerzak4@gmail.com>
2023-08-04 01:55:05 +0200

    my_code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 
-		number = (number * 2) + (b[i] - '0');
+		result = (result * 2) + (b[i] - '0');
 		result += (b[i] - '0') * base;
 		base *= 2;
 	}
