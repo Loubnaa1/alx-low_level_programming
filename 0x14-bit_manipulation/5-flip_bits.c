adfdf87b8f970c5142270a00532fa5bc2c7ea90c
Loubnaa1 <loubnamerzak4@gmail.com>
2023-08-04 02:28:27 +0200

    my_code

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * flip_bits - returns the number of bits needed to flip to get from n to m
- * @n: numberone
- * @m: numbertwo
- * Return:the bits to flip
+ * flip_bits - Counts the number of bits
+ * needed to flip to get from n to m.
+ * @n: The first number.
+ * @m: The second number.
+ * Return: The number of bits to flip.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
