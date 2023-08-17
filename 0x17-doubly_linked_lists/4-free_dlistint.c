627ce3d4e1780a34d7c4a85f09263c8596e79a2c
Loubnaa1 <loubnamerzak4@gmail.com>
2023-08-17 16:50:58 -0700

    my_code

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,14 +1,13 @@
 #include "lists.h"
 /**
- * free_dlistint - function that
- * frees the list.
- * @head: pointer to the head of the list.
- * Return: void 
+ * free_dlistint - function that frees the list.
+ * @head: pointer to pointer.
+ * Return: void.
  */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *h;
-	
+
 	while (head != NULL)
 	{
 		h = head;
--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 /**
- * get_dnodeint_at_index - returns node.
- * @head: pointer to head node.
- * @index: index of node to return.
- *
- * Return: pointer or NULL. 
+ * get_dnodeint_at_index - function that
+ * returns the n node of  list.
+ * @head: pointer to the head.
+ * @index: index of the node.
+ * Return: pointer or NULL.
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *h = head;
 	unsigned int i = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		if (i == index)
 			return (h);
