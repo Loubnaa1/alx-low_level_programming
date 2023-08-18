3be8be1c1268ff4ed8a1c6a12b5e896d5443e1fd
Loubnaa1 <loubnamerzak4@gmail.com>
2023-08-17 17:24:42 -0700

    my_code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,40 +1,42 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - function that inserts
- * a node at a given index.
- * @h: pointer to  pointer.
- * @idx : index where to insert.
- * @n: value of inserted node.
- * Return: new node or NULL if failed.
+ * insert_dnodeint_at_index - function that inserts a new node at
+ * a given position
+ * @h: pointer to structure pointer
+ * @idx : index where the new node should be added
+ * @n: data
+ * Return: new node or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 1;
-	dlistint_t *previous = *h;
-	dlistint_t *new = NULL;
+	dlistint_t *tracker = *h;
+	dlistint_t *new_n = NULL;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	previous = previous->next;
-	for (i = 0; previous != NULL; i++, previous = previous->next)
+	tracker = tracker->next;
+	while (tracker != NULL)
 	{
 		if (idx == i)
 		{
 			new_n = malloc(sizeof(dlistint_t));
 			if (new_n == NULL)
 				return (NULL);
-			new->next = previous;
-			new->n = n;
-			new->prev = previous->prev;
-			previous->prev->next = new;
-			previous->prev = new;
-			return (new);
+			new_n->next = tracker;
+			new_n->n = n;
+			new_n->prev = tracker->prev;
+			tracker->prev->next = new_n;
+			tracker->prev = new_n;
+			return (new_n);
 		}
+		i++;
+		tracker = tracker->next;
 	}
 	if (idx == i)
 	{
 		return (add_dnodeint_end(h, n));
 	}
-	free(new);
+	free(new_n);
 	return (NULL);
 }
