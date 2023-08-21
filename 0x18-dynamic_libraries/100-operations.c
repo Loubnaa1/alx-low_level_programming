8132f41b4868d8a9c74cff5183a35b28cc1eaac0
Loubnaa1 <loubnamerzak4@gmail.com>
2023-08-21 16:36:59 -0700

    my_code

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int add(int i, int j)
+{
+	return (i + j);
+}
+int sub(int i, int j)
+{
+	return (i - j);
+}
+int mul(int i, int j)
+{
+	return (i * j);
+}
+int div(int i, int j)
+{
+	return (i / j);
+}
+int mod(int i, int j)
+{
+	return (i % j);
+}
