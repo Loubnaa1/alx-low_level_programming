cd2c20dd77beef8b3528e1c15471659da1d415d4
Loubnaa1 <loubnamerzak4@gmail.com>
2023-12-16 18:42:07 -0800

    project

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,1 +1,25 @@
-ii
+#include "search_algos.h"
+
+/**
+ * linear_search - function that searches a value in
+ * an array of integers using the Linear search algorithm
+ * @array: pointer to first element
+ * @size: size of elements of the array
+ * @value: value to search 
+ * Return: first value located or -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,1 +1,42 @@
-i
+#include "search_algos.h"
+
+/**
+ * binary_search - This function implements the binary search algorithm on a 
+ * sorted array. It searches for a specific value and returns its index.
+ * @array: A pointer to the first element of the array.
+ * @value: The value to search
+ * @size: The size of the array.
+ * Return: The index where value is located.
+ * if array is NULL, returns -1.
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+    int link = 0, recht = (int)size - 1, middle, i;
+
+    if (!array)
+        return (-1);
+
+    for (; link <= recht;)
+    {
+        middle = (recht + link) / 2;
+        printf("Searching in array: ");
+        for (i = link; i <= recht; i++)
+        {
+            printf("%d", array[i]);
+            if (i < recht)
+                printf(", ");
+        }
+        printf("\n");
+
+        if (array[middle] == value)
+            return (middle);
+        else if (array[middle] < value)
+            link = middle + 1;
+        else
+            recht = middle - 1;
+    }
+
+    return (-1);
+}
+
